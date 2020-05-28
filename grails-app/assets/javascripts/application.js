88a7d7fef290027bb150bc259a73ee70ba64cdb1
PShanka00 <prabhash.shankar@ingrammicro.com>
2020-05-28 12:33:36 +0530

    change in Interceptor class

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,7 +5,8 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require jquery-3.3.1.min
+//= require jquery-3.4.1.min
 //= require bootstrap
 //= require popper.min
 //= require_self
+//= require escm_alert
